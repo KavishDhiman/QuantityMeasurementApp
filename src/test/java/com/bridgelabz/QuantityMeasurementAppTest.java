145eaba2d4b485bba742b4bf07d9bbef293a67ab
KavishDhiman <kavishdhiman2003@gmail.com>
2026-02-25 14:03:37 +0530

    UC1: Implement Feet measurement equality with equals contract

--- src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
+++ src/test/java/com/bridgelabz/QuantityMeasurementAppTest.java
@@ -6,7 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuantityMeasurementAppTest {
 
     @Test
-    void sampleTest() {
-        assertEquals(2, 1 + 1);
+    void givenSameValueInFeet_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(feet1.equals(feet2));
+    }
+
+    @Test
+    void givenDifferentValueInFeet_whenCompared_shouldReturnFalse() {
+        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(feet1.equals(feet2));
+    }
+
+    @Test
+    void givenNull_whenCompared_shouldReturnFalse() {
+        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(feet.equals(null));
+    }
+
+    @Test
+    void givenSameReference_whenCompared_shouldReturnTrue() {
+        QuantityMeasurementApp.Feet feet = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(feet.equals(feet));
     }
 }
