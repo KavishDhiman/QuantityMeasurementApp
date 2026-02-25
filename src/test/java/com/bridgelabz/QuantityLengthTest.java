fd79683f3f994d126ca9c6312d515b9def1ef1dc
KavishDhiman <kavishdhiman2003@gmail.com>
2026-02-25 14:50:06 +0530

    UC4: Add YARDS and CENTIMETERS support via enum extension (scalable design)

--- src/test/java/com/bridgelabz/QuantityLengthTest.java
+++ src/test/java/com/bridgelabz/QuantityLengthTest.java
@@ -65,4 +65,62 @@ class QuantityLengthTest {
 
         assertTrue(q.equals(q));
     }
+    @Test
+    void testYardToYard_SameValue() {
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength q2 = new QuantityLength(1.0, LengthUnit.YARDS);
+
+        assertTrue(q1.equals(q2));
+    }
+
+    @Test
+    void testYardToFeet_EquivalentValue() {
+        QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
+
+        assertTrue(yard.equals(feet));
+        assertTrue(feet.equals(yard));
+    }
+
+    @Test
+    void testYardToInch_EquivalentValue() {
+        QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength inch = new QuantityLength(36.0, LengthUnit.INCH);
+
+        assertTrue(yard.equals(inch));
+    }
+    @Test
+    void testCentimeterToCentimeter_SameValue() {
+        QuantityLength c1 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
+        QuantityLength c2 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
+
+        assertTrue(c1.equals(c2));
+    }
+
+    @Test
+    void testCentimeterToInch_EquivalentValue() {
+        QuantityLength cm = new QuantityLength(1.0, LengthUnit.CENTIMETERS);
+        QuantityLength inch = new QuantityLength(0.393701, LengthUnit.INCH);
+
+        assertTrue(cm.equals(inch));
+    }
+
+    @Test
+    void testCentimeterToFeet_NonEquivalent() {
+        QuantityLength cm = new QuantityLength(1.0, LengthUnit.CENTIMETERS);
+        QuantityLength feet = new QuantityLength(1.0, LengthUnit.FEET);
+
+        assertFalse(cm.equals(feet));
+    }
+    @Test
+    void testMultiUnit_TransitiveProperty() {
+
+        QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARDS);
+        QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
+        QuantityLength inch = new QuantityLength(36.0, LengthUnit.INCH);
+
+        assertTrue(yard.equals(feet));
+        assertTrue(feet.equals(inch));
+        assertTrue(yard.equals(inch));
+    }
 }
